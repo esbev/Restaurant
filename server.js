efc5e7a3b99b43f522e980bfa6aa4085f980e9bd
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-08-10 20:34:37 -0500

    Middleware work Adjusted pattern of middleware for seeding db to be effective.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const session = require("express-session");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers");
-const path = require("path");
 // const helpers = require('./utils/helpers');
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,24 +13,26 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create();
 
 const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(session(routes));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.use(express(routes));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
